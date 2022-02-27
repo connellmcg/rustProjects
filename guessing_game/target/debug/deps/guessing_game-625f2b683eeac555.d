5967abb6e524bade10c24b75a98ba754a46b744e
Connell McGinley <connell@Connells-MacBook-Air.local>
2022-02-27 20:21:44 +0000

    added comments

--- guessing_game/target/debug/deps/guessing_game-625f2b683eeac555.d
+++ guessing_game/target/debug/deps/guessing_game-625f2b683eeac555.d
@@ -1,5 +1,5 @@
-/Users/connell/projects/guessing_game/target/debug/deps/guessing_game-625f2b683eeac555: src/main.rs
+/Users/connell/rustProjects/guessing_game/target/debug/deps/guessing_game-625f2b683eeac555: src/main.rs
 
-/Users/connell/projects/guessing_game/target/debug/deps/guessing_game-625f2b683eeac555.d: src/main.rs
+/Users/connell/rustProjects/guessing_game/target/debug/deps/guessing_game-625f2b683eeac555.d: src/main.rs
 
 src/main.rs:
--- guessing_game/target/debug/guessing_game.d
+++ guessing_game/target/debug/guessing_game.d
@@ -1,1 +1,1 @@
-/Users/connell/projects/guessing_game/target/debug/guessing_game: /Users/connell/projects/guessing_game/src/main.rs
+/Users/connell/rustProjects/guessing_game/target/debug/guessing_game: /Users/connell/rustProjects/guessing_game/src/main.rs
