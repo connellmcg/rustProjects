329f0c2f21b1d088c97189aa47ffd36a6baaab98
Connell McGinley <connell@Connells-MacBook-Air.local>
2022-02-28 21:59:27 +0000

    tonights work

---
+++ ch5-2_rectangles_structs/target/rls/debug/deps/ch5_2_rectangles_structs-2548a7f5c3b90f85.d
@@ -0,0 +1,5 @@
+/Users/connell/rustProjects/ch5-2_rectangles_structs/target/rls/debug/deps/ch5_2_rectangles_structs-2548a7f5c3b90f85.rmeta: src/main.rs
+
+/Users/connell/rustProjects/ch5-2_rectangles_structs/target/rls/debug/deps/ch5_2_rectangles_structs-2548a7f5c3b90f85.d: src/main.rs
+
+src/main.rs:
---
+++ ch5-2_rectangles_structs/target/rls/debug/deps/ch5_2_rectangles_structs-87c7271adad9cfe4.d
@@ -0,0 +1,5 @@
+/Users/connell/rustProjects/ch5-2_rectangles_structs/target/rls/debug/deps/ch5_2_rectangles_structs-87c7271adad9cfe4.rmeta: src/main.rs
+
+/Users/connell/rustProjects/ch5-2_rectangles_structs/target/rls/debug/deps/ch5_2_rectangles_structs-87c7271adad9cfe4.d: src/main.rs
+
+src/main.rs:
